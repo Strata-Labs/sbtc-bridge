52308d6629fa533c9665c3e5c27f9a0bc8f96836
hz <gravatar.m2a16@simplelogin.com>
2024-11-28 16:39:08 +0200

    fix runtime issue after build
    layout suspense wrapping a server component broke the app, def need to switch that too to a server component as recommended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@
 
 import getSbtcBridgeConfig from "@/actions/get-sbtc-bridge-config";
 import HomeApp from "@/comps/HomeApp";
+import { Suspense } from "react";
 
 export default async function Home() {
   const sBTCBridgeConfig = await getSbtcBridgeConfig();
   return (
     <main className="min-w-screen bg-white  flex items-center flex-col min-h-screen ">
-      <HomeApp config={sBTCBridgeConfig} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HomeApp config={sBTCBridgeConfig} />
+      </Suspense>
     </main>
   );
 }
