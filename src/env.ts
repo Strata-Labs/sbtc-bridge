f68f5916f2ef732e042902d4d357e5704bf3a54f
hz <gravatar.m2a16@simplelogin.com>
2024-11-29 18:42:39 +0200

    add default network env variable

--- src/env.ts
+++ src/env.ts
@@ -8,7 +8,7 @@ export const env = {
     process.env.MEMPOOL_API_URL || "http://localhost:8083/api/v1/",
   BITCOIN_RPC_USER_NAME: process.env.BITCOIN_RPC_USER_NAME || "devnet",
   BITCOIN_RPC_PASSWORD: process.env.BITCOIN_RPC_PASSWORD || "devnet",
-  WALLET_NETWORK: process.env.WALLET_NETWORK,
+  WALLET_NETWORK: process.env.WALLET_NETWORK || "sbtcDevenv",
   SBTC_CONTRACT_DEPLOYER: process.env.SBTC_CONTRACT_DEPLOYER,
   SIGNER_AGGREGATE_KEY: process.env.SIGNER_AGGREGATE_KEY,
 };
